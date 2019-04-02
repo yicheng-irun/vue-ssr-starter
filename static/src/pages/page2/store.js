8a71d6cfb27c61dffcc382ef3b286cbff8c96259
yicheng <yicheng-me@qq.com>
2019-04-03 07:29:34 +0800

    修改store为async函数

--- static/src/pages/page2/store.js
+++ static/src/pages/page2/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 import { get } from '@/lib/ajax.js';
+// import runtime from '@/lib/runtime.js';
 
 
 export default function () {
@@ -22,32 +23,27 @@ export default function () {
         },
 
         actions: {
-            fetchItem ({ commit }) {
-                return new Promise((resolve, reject) => {
-                    // 这里可以请求 后台cgi 数据
-                    get('/api/demo', { sayhi: 'hi server!' }).then((rsp) => {
-                        const result = rsp.data;
-                        commit('setData', result.data);
-                        resolve();
-                    }).catch(reject);
-                });
+            async fetchItem ({ commit }) {
+                // 这里可以请求 后台cgi 数据
+                const rsp = await get('/api/demo', { sayhi: 'hi server!' });
+                const result = rsp.data;
+                commit('setData', result.data);
             },
         },
     });
 
 
     return {
-        serverFetch () { // 服务端 拉取数据
-            return store.dispatch('fetchItem');
+        async serverFetch () { // 服务端 拉取数据
+            // runtime.action.next();
+            return await store.dispatch('fetchItem');
         },
-        clientFetch () { // 客户端 拉取数据
+        async clientFetch () { // 客户端 拉取数据
             if (window.__INITIAL_STATE__) {
-                return new Promise((resolve) => {
-                    store.replaceState(window.__INITIAL_STATE__);
-                    resolve();
-                });
+                store.replaceState(window.__INITIAL_STATE__);
+                return;
             }
-            return store.dispatch('fetchItem'); // 拉取数据
+            return await store.dispatch('fetchItem'); // 拉取数据
         },
         instance: store,
     };
