e9d6c62188f78814c3fbf3adc3d634c5f8ed325b
yicheng <yicheng-me@qq.com>
2019-04-03 07:32:12 +0800

    改create-app为async函数

--- static/src/create-app.js
+++ static/src/create-app.js
@@ -3,22 +3,19 @@ import runtime from './lib/runtime';
 import pages from './pages';
 
 
-export default function createApp () {
-    return new Promise((resolve, reject) => {
-        const page = runtime.page;
-        let f = pages[page];
-        if (!f) {
-            f = pages.default;
-        }
+export default async function createApp () {
+    const page = runtime.page;
+    let f = pages[page];
+    if (!f) {
+        f = pages.default;
+    }
 
-        f().then((factfunc) => {
-            const {App, Store} = factfunc.default;
-            const store = Store();
-            const app = new Vue({
-                store: store.instance,
-                render: h => h(App),
-            });
-            resolve({ app, store });
-        }).catch(reject);
+    const factfunc = await f();
+    const {App, Store} = factfunc.default;
+    const store = Store();
+    const app = new Vue({
+        store: store.instance,
+        render: h => h(App),
     });
+    return { app, store };
 }
